ee610c857d26bd11bc99e429d52f59370e9f3cd3
k1ns3 <43415209+k1ns3@users.noreply.github.com>
2020-04-29 17:05:27 +0300

    [scout-library-login] Убрал дату, гитлаб токен. Исправил инпуты

--- src/app/scout-library-login/scout-library-login.module.ts
+++ src/app/scout-library-login/scout-library-login.module.ts
@@ -3,11 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ScoutLibraryLoginComponent } from './scout-library-login.component';
+import {
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
+} from '@angular/material';
+
 
 @NgModule({
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        MatButtonModule
     ],
     declarations: [
         ScoutLibraryLoginComponent,
